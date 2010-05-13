fbb87e294c59b7b3d28047f160a87f10cdffd327
unknown <hedger@.(none)>
2010-05-14 00:02:10 +0400

    initial commit

---
+++ build.bat
@@ -0,0 +1,4 @@
+rm pror.smx "/cygdrive/d/Games/Steam/steamapps/tehhedger/source 2007 dedicated server/tf/addons/sourcemod/plugins/pror.smx"
+d:\projects\sm\turbo\smsrc\addons\sourcemod\scripting\spcomp.exe pror.sm
+cp pror.smx "/cygdrive/d/Games/Steam/steamapps/tehhedger/source 2007 dedicated server/tf/addons/sourcemod/plugins/"
+pause
